d687a17166d7a618cd9ca1103e06467a7e46b7b4
Vladimir Gamalyan <vladimir.gamalian@gmail.com>
2021-11-12 15:02:49 +0300

    Add doctest

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+    {
+        doctest::Context context;
+        context.applyCommandLine(argc, argv);
+        int res = context.run();
+        if (context.shouldExit())
+            return res;
+    }
+
+    return 0;
+}
+
+TEST_CASE("foo test")
+{
+    CHECK(2 * 2 == 4);
 }
